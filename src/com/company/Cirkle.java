a75b7538583e36cbd923376cbef224c784d823c8
Ulukbek <ulukbekkochkorov@gmail.com>
2022-05-16 22:35:14 +0600

    Initial commit

---
+++ src/com/company/Cirkle.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Cirkle {
+    static final double PI = 3.14159;
+    static int r = 5;
+    static double a;
+    static double c;
+
+    public static double area (){
+        a = PI * (r*r);
+        return a;
+    }
+    public static double circumference (){
+        c = PI * 2 * r;
+        return c;
+    }
+}
